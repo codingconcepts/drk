8cada0efc656858139eec6ea53edefde399c79f3
Rob Reid <robreid@crlMBP-NHJNH36CFJMjY2.local>
2023-02-26 21:01:54 +0000

    feat: Better description for z

--- drk.go
+++ drk.go
@@ -20,7 +20,7 @@ func main() {
 	n := flag.Int("n", 200, "number of requests to run, can't be less than c")
 	c := flag.Int("c", 1, "number of workers to run concurrently, can't be more than n")
 	q := flag.Int64("q", 0, "number of queries to run per second")
-	z := flag.Duration("z", 0, "duration of test")
+	z := flag.Duration("z", 0, "duration of test, n is ignored if z is specified")
 	flag.Parse()
 
 	if *n < *c {
