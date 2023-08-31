0089ace41aefd484db399e960ed971a4080a9fb0
codingconcepts <hello@robreid.io>
2023-08-31 08:30:40 +0100

    chore: Refactor row scanning

--- internal/pkg/runner/runner.go
+++ internal/pkg/runner/runner.go
@@ -43,27 +43,9 @@ func (r *Runner) Query(table, stmt string, args []any) error {
 		return fmt.Errorf("making query: %w", err)
 	}
 
-	// Read everything from the table into memory.
-	fields := dbRows.FieldDescriptions()
-
-	rows := []map[string]any{}
-	for dbRows.Next() {
-		scans := make([]any, len(fields))
-		for i := range scans {
-			scans[i] = &scans[i]
-		}
-
-		if err = dbRows.Scan(scans...); err != nil {
-			return fmt.Errorf("scaning values: %w", err)
-		}
-
-		row := make(map[string]any)
-		for i, v := range scans {
-			if v != nil {
-				row[fields[i].Name] = v
-			}
-		}
-		rows = append(rows, row)
+	rows, err := read(dbRows)
+	if err != nil {
+		return fmt.Errorf("scanning rows: %v", err)
 	}
 
 	// Add to the runner's in-memory cache of data.
